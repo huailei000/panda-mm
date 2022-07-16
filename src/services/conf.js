b29f5b20c40bf59452e59782bdcd09245e6d739c
v-hleihuai <v-hleihuai.tencent.com>
2022-07-16 21:26:19 +0800

    feat: edit

--- src/services/conf.js
+++ src/services/conf.js
@@ -1,9 +1,9 @@
-let host = 'localhost:9008';
-let username = 'panda@megvii.com';
+let host = 'localhost:9000';
+let username = 'panda.com';
 let password = '123456';
 
 // let host = '10.169.0.143'
-// let username = 'lxj@megvii.com'
+// let username = 'lxj.com'
 // let password = '123456'
 
 host = process.env.DEPLOY == 'release' ? window.location.host : host;
